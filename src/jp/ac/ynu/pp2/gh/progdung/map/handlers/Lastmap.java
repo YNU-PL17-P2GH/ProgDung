6b9126bb0afd26afd0eeb71ed6b9bf09fab221b1
SHIGAYUYA <shiga-yuya-yr@ynu.jp>
2017-07-12 01:32:59 +0900

    lastmap修正

--- src/jp/ac/ynu/pp2/gh/progdung/map/handlers/Lastmap.java
+++ src/jp/ac/ynu/pp2/gh/progdung/map/handlers/Lastmap.java
@@ -2,12 +2,25 @@ package jp.ac.ynu.pp2.gh.progdung.map.handlers;
 
 import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MAP_CONST.DIRECTION;
 import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MapHandlerBase;
+import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MapObject;
 import jp.ac.ynu.pp2.gh.progdung.gui.DungeonPlay;
 
 public class Lastmap extends MapHandlerBase {
-	
+
 	public Lastmap(int player_x, int player_y, DIRECTION player_d, DungeonPlay play) {
 		super("lastmap", player_x, player_y, player_d, play);
+	}
+
+	@Override
+	public void playerUpdate() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void onPlayerHitTo(MapObject object) {
+		// TODO 自動生成されたメソッド・スタブ
+
 	}
 
 }
