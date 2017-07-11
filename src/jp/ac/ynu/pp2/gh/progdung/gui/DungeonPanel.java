6ba04637db7913c9d63c516449d639ab7b9ff7a7
HRSNOZOMU <hirose-nozomu-jr@ynu.jp>
2017-07-11 15:50:34 +0900

    とりあえず

--- src/jp/ac/ynu/pp2/gh/progdung/gui/DungeonPanel.java
+++ src/jp/ac/ynu/pp2/gh/progdung/gui/DungeonPanel.java
@@ -1,10 +1,13 @@
 package jp.ac.ynu.pp2.gh.progdung.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.OverlayLayout;
 
 import jp.ac.ynu.pp2.gh.progdung.util.TransitionCallback;
 
@@ -21,18 +24,27 @@ public class DungeonPanel extends JPanel {
 	
 	public DungeonPanel(TransitionCallback pCallback) {
 		super();
-
+		
 		callback = pCallback;
-		setLayout(new BorderLayout());
+		setLayout(new OverlayLayout(this));
+		
+		// TODO ヒントとか出したい
+		JPanel lpanel = new JPanel();
+//		lpanel.setPreferredSize(new Dimension(300, 100));
+//		lpanel.setBackground(Color.blue);
+//		lpanel.setAlignmentX(java.awt.Component.LEFT_ALIGNMENT);
+		add(lpanel);
 		
 		JPanel lPlayCoverPanel = new JPanel();
 		FlowLayout lFlow1 = new FlowLayout();
 		lFlow1.setAlignment(FlowLayout.LEFT);
 		lPlayCoverPanel.setLayout(lFlow1);
+		lPlayCoverPanel.setOpaque(false);
 		lPlayCoverPanel.setBorder(BorderFactory.createEmptyBorder(10, 640-352, 10, 0));
 		lDungeonPlay = new DungeonPlay(callback);
 		lPlayCoverPanel.add(lDungeonPlay);
 		add(lPlayCoverPanel, BorderLayout.CENTER);
+		
 	}
 	
 	public void init() {
