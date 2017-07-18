b869df81479637edbb4034e43837af91c7eacdaf
yuya <yuya@DESKTOP-HSR8EJP>
2017-07-19 00:24:03 +0900

    shortest追加&clearマップNQueen以外追加

---
+++ src/jp/ac/ynu/pp2/gh/progdung/map/handlers/Knight2_clear.java
@@ -0,0 +1,32 @@
+package jp.ac.ynu.pp2.gh.progdung.map.handlers;
+
+import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MAP_CONST.DIRECTION;
+import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MapHandlerBase;
+import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MapObject;
+import jp.ac.ynu.pp2.gh.progdung.gui.DungeonPlay;
+
+public class Knight2_clear extends MapHandlerBase {
+
+	public Knight2_clear(int player_x, int player_y, DIRECTION player_d, DungeonPlay play) {
+		super("knight2_clear", player_x, player_y, player_d, play);
+	}
+
+	@Override
+	public void playerUpdate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPlayerHitTo(MapObject object) {
+	}
+
+	@Override
+	public void onPlayerInteract(MapObject pObject) {
+	}
+
+	@Override
+	public void onMapLoad() {
+	}
+}
+
---
+++ src/jp/ac/ynu/pp2/gh/progdung/map/handlers/Meiro_clear.java
@@ -0,0 +1,31 @@
+package jp.ac.ynu.pp2.gh.progdung.map.handlers;
+
+import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MAP_CONST.DIRECTION;
+import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MapHandlerBase;
+import jp.ac.ynu.pp2.gh.naclo.mapseq.map.MapObject;
+import jp.ac.ynu.pp2.gh.progdung.gui.DungeonPlay;
+
+public class Meiro_clear extends MapHandlerBase {
+
+	public Meiro_clear(int player_x, int player_y, DIRECTION player_d, DungeonPlay play) {
+		super("meiro_clear", player_x, player_y, player_d, play);
+	}
+
+	@Override
+	public void playerUpdate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPlayerHitTo(MapObject object) {
+	}
+
+	@Override
+	public void onPlayerInteract(MapObject pObject) {
+	}
+
+	@Override
+	public void onMapLoad() {
+	}
+}
