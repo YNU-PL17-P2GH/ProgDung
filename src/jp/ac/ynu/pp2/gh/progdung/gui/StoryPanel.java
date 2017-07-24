e69177455c741380481b63ea0a0af3fa81a7b560
yuya <yuya@DESKTOP-HSR8EJP>
2017-07-25 00:15:06 +0900

    ストーリー追加

--- src/jp/ac/ynu/pp2/gh/progdung/gui/StoryPanel.java
+++ src/jp/ac/ynu/pp2/gh/progdung/gui/StoryPanel.java
@@ -1,22 +1,26 @@
 package jp.ac.ynu.pp2.gh.progdung.gui;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 import jp.ac.ynu.pp2.gh.progdung.util.TransitionCallback;
 
 public class StoryPanel extends BackgroundedPanel {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -4391381659704172647L;
-	
+
 	public StoryPanel(final TransitionCallback pCallback) {
 		super();
 		try {
@@ -24,26 +28,45 @@ public class StoryPanel extends BackgroundedPanel {
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		
+		setBorder(BorderFactory.createEmptyBorder(80, 0, 0, 0));
 		FlowLayout layout1 = new FlowLayout();
-		layout1.setVgap(50);
+		layout1.setVgap(20);
 		layout1.setHgap(120);
-		layout1.setAlignment(FlowLayout.TRAILING);
+		layout1.setAlignment(FlowLayout.CENTER);
 		setLayout(layout1);
-		
+
 		// TODO 適当にストーリーを書き込む
+		Font font = new Font(Font.MONOSPACED, Font.PLAIN, 32);
+		JLabel storyText = new JLabel("<html><Div Align=\"center\">ここは過去に崩壊した文明の遺跡...<br>"
+				+ "今よりもはるかに進んだその文明が作り上げたその『迷宮』は<br>"
+				+ "その文明が滅び、すべて終わりを迎えてもなお<br>"
+				+ "その役割を忘れず、侵入者を拒む...<br>"
+				+ "あなたはその『迷宮』の謎を解き明かし<br>"
+				+ "その文明の英知を我がものとしようとする冒険者の一人<br>"
+				+ "あなたに果たしてこの『迷宮』を解き明かし<br>"
+				+ "英知をその手にすることができるのでしょうか<br>"
+				+ "あなたの冒険が今幕を上げる...</Div></html>");
+		storyText.setFont(font);
+		storyText.setForeground(Color.LIGHT_GRAY);
+		Font lfont = new Font(Font.MONOSPACED, Font.PLAIN, 42);
+		JLabel storySubText = new JLabel("<html>あなたを阻むその『迷宮』の名は「<font color=\"red\">ALGEON</font>」</html>") ;
+		storySubText.setFont(lfont);
+		storySubText.setForeground(Color.WHITE);
 		
 		JButton lButton = new JButton("進む");
 		lButton.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				pCallback.showSelect(pCallback);
 			}
 		});
-		
+		add(storyText);
+		add(new Separator());
+		add(storySubText);
+		add(new Separator());
 		add(lButton);
-		
+
 	}
 
 }
