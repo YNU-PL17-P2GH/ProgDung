d34735cd7b468aaa215c2898ab14a2f277a5f7ed
HRSNOZOMU <hirose-nozomu-jr@ynu.jp>
2017-07-24 14:46:29 +0900

    Add server connection

--- src/jp/ac/ynu/pp2/gh/progdung/util/Connection.java
+++ src/jp/ac/ynu/pp2/gh/progdung/util/Connection.java
@@ -1,28 +1,50 @@
 package jp.ac.ynu.pp2.gh.progdung.util;
 
+
+
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Connection {
 	
 	public static Socket mySocket;
 	public static final String SERVER_ADDR = "localhost";
-	public static final int SERVER_PORT = 50000;
+	public static final int SERVER_PORT = 9999;
 	
-	public static void init() {
+	public void init() {
 		try {
 			mySocket = new Socket(SERVER_ADDR, SERVER_PORT);
+			System.out.println("socket was created");
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 	
-	public static void sendObject(Object pObject) {
+	public void sendObject(Object pObject) {
+		ObjectOutputStream oos;
+		try {
+			oos = new ObjectOutputStream(mySocket.getOutputStream());
+			oos.writeObject(pObject);
+				} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public static void receiveObject(Object pObject) {
+	public Object receiveObject() {
+		ObjectInputStream ois;
+		try {
+			ois = new ObjectInputStream(mySocket.getInputStream());
+			return ois.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return null;
 		
 	}
-
+	
 }
---
+++ src/jp/ac/ynu/pp2/gh/progdung/util/Server.java
@@ -0,0 +1,37 @@
+package jp.ac.ynu.pp2.gh.progdung.util;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server{
+	private static int port=9999; // サーバの待ち受けポート
+	
+	public Server(int port){
+		Server.port = port; //待ち受けポートを渡す
+	}
+	
+	public static void main(String[] args){
+		ServerSocket serverSocket = null;
+		try{
+			serverSocket = new ServerSocket(port);
+			System.out.println("Server Ready");
+			while(true){						//常にクライアントとの新規接続を待つ
+				Socket socket = serverSocket.accept();
+				new Receiver(socket).start();	//socketスレッドを起動
+				System.out.println("thread start");
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally {
+		      try {
+		          if (serverSocket != null) {
+		            serverSocket.close();
+		          }
+		        } catch (IOException e) {}
+		}
+	}
+	
+}
+
